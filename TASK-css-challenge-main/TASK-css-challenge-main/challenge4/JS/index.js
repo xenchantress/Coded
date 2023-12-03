a9536b1fcdb5b70becc49872c09be9dabbd86c01
THE LUJAIN <silversocer17@gmail.com>
2023-12-03 19:15:19 +0300

    p2

---
+++ TASK-css-challenge-main/TASK-css-challenge-main/challenge4/JS/index.js
@@ -0,0 +1,11 @@
+let budget = 1000;
+let cost = 750.250;
+
+budget= 750;
+cost = 500;
+
+budget= `cost`;
+username = `Allujain`
+console.log(`Salam`);
+console.log(`${username}`)
+console.log(`this messege is for ${username}`);
